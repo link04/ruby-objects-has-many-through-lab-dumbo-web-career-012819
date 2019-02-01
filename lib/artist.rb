7eb78c942c249332c7e11e050b59ffb9ab24e3ee
Maximo Bautista <max.bautista97@gmail.com>
2019-02-01 20:53:20 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -18,9 +18,16 @@ class Artist
   end
   
    def songs
-     
-     
-    Song.new(name, self, genre )
+
+    Appointment.all.select do |appoinment|
+      appoinment.doctor == self
+    end
+  end
+
+  def patients
+    self.appointments.map { |appoinment| appoinment.patient }
+  end
+
   end
  
 end
